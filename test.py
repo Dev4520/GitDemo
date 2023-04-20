a00ac3f3bf8f7c6126e7d2b9a13ed5e6efbdc793
Devanshu Shekhar <72151489+Dev4520@users.noreply.github.com>
2023-04-20 11:36:53 +0530

    Update test.py

--- test.py
+++ test.py
@@ -2,7 +2,7 @@ print("this is test file") #:- written by D1
 print("This line added form github") #:- written by D2
 
 print("Line1")
-print("Line2")
+print("This is developer two Line2")
 print("Line3")
 print("Line4")
 print("Line5")
