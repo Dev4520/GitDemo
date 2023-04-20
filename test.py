36252c6dc74d5fdb67f7241839f9fe521ab97985
Dev4520 <shekhar4520@gmail.com>
2023-04-20 11:35:25 +0530

    Update demo.py

--- test.py
+++ test.py
@@ -2,8 +2,12 @@ print("this is test file") #:- written by D1
 print("This line added form github") #:- written by D2
 
 print("Line1")
-print("Line2")
+print("This is Line2")
 print("Line3")
 print("Line4")
 print("Line5")
 print("Line6")
+
+
+# condition for merge conflict is that, the change should coccur in the same file and same line
+
